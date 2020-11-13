bec67e72e11b326e3983d240685787b7bedaab9f
nobody48sheldor <69438501+nobody48sheldor@users.noreply.github.com>
2020-11-13 22:45:56 +0100

    Update fuseeinator4.py

--- fuseeinator4.py
+++ fuseeinator4.py
@@ -1,10 +1,8 @@
 from mpl_toolkits.mplot3d import axes3d
 import matplotlib.pyplot as plt
 from matplotlib import cm
-import os
 import numpy as np
 from math import *
-
 
 g = 9.8
 m = 1.2
@@ -24,24 +22,51 @@ k = (Co * K * rho * pi * r * sqrt((r*r) + (h*h)))
 n = int(input("n = "))
 dt = 1/n
 
-def high(yo):
+fig = plt.figure()
+ax = fig.add_subplot(111, projection = '3d')
+
+def high(yo, tm):
     i = 0
-    high = []
-    while i < n:
-        print("[", i ,"/", n, "]", int((i/n)*100), "%")
-        h = 0
-        yp = yo[i]
-        yr = yo[i]
-        while yp > 0:
-            yp = yr + dt*(-(k/m)*(yr**3/abs(yr))-g)
-            yr = yp
-            h = h + yr*dt
-        high.append(h)
-        i = i + 1
-    return(high)
+    j = 0
+    hight = []
+    high= []
+    while j < n:
+        yoj = yo[j][j]
+        i = 0
+        print(int((j/n)*100), "%")
+        print("\n"*22)
+        high = []
+        while i < n:
+            t = 0
+            h = 0
+            yp = yoj
+            yr = yoj
+            tmi = tm[i][1]
+            while t <= tmi:
+                yp = yr + dt*(-(k/m)*(yr**3/abs(yr))-g)
+                yr = yp
+                h = h + yr*dt
+                t = t + dt
+                if h < 0:
+                    h = 0
+            high.append(h)
+            i = i + 1
+        hight.append(high)
+        j = j + 1
+    hightF = np.array(hight)
+    return(hightF)
 
-x = np.linspace(0, 1000, n)
-y = high(x)
+X = np.linspace(0.0001, 400, n)
+Y = np.linspace(0.0001, 38, n)
 
-plt.plot(x, y)
+x, y = np.meshgrid(X, Y)
+
+Z = high(x,y)
+
+ax.plot_surface(y, x, Z, cmap = cm.plasma, linewidth=0, antialiased=True)
+
+ax.set_xlabel('vfp')
+ax.set_ylabel('t')
+ax.set_zlabel('h')
+
 plt.show()
